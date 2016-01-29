51d06941d58e30ffd1cd91c945a755cabdeb0893
Renan T. Carneiro <renanteruoc@gmail.com>
2016-01-29 20:09:44 -0200

    roomobjects

--- scenes/dungeon.lua
+++ scenes/dungeon.lua
@@ -9,6 +9,8 @@ local map
 
 -- local rooms = require "rooms"
 
+-- local room = require "room"
+
 local roomexits = {'E','S','W','W','N','E'}
 local roomentries = {'W','W','N','E','E','S'}
 local roomnumber = 1
@@ -17,6 +19,17 @@ playerstartingpos['W'] = {2, H/2}
 playerstartingpos['E'] = {W-1, H/2}
 playerstartingpos['N'] = {W/2, 2}
 playerstartingpos['S'] = {W/2, H-1}
+
+roomobjects = {
+  {},
+  {},
+  {},
+  {},
+  {},
+  {},
+}
+
+local activeobjects = {}
 
 local function updateroom()
     for i=1,H do
@@ -77,11 +90,24 @@ end
 function dungeon.update (dt)
   player.update(dt)
   local playerpos = player.getpos()
+
+  for _,obj in ipairs(activeobjects) do
+    obj.update(dt)
+  end
+
+  --REMINDER: ULTIMA COISA A ACONTECER KTHXBYE
   if playerpos[1] < 0 or playerpos[2] < 0 or
     playerpos[1] > W or playerpos[2] > H then
     roomnumber = roomnumber + 1
     if roomnumber > #roomexits then
       roomnumber = 1
+    end
+    for i = #activeobjects,1,-1 do
+      activeobjects[i] = nil
+    end
+    for _,obj in ipairs(roomobjects[roomnumber]) do
+      table.insert(activeobjects, obj)
+      obj.load()
     end
     updateroom()
     player.setpos(vec2:new(playerstartingpos[roomentries[roomnumber]]))

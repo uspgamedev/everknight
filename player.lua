029b9ef52bf95493bf5c0d1841f58a2c933738e2
Renan T. Carneiro <renanteruoc@gmail.com>
2016-01-29 22:57:14 -0200

    player health

--- player.lua
+++ player.lua
@@ -13,6 +13,9 @@ local angle
 local spd
 local moving
 
+local health = 10
+local damage = 0
+
 function player.clear ()
   pos = vec2:new{}
   angle = 0
@@ -31,7 +34,28 @@ function player.getmove ()
   return (moving and spd or 0)*vec2:new{ math.cos(angle), math.sin(angle) }
 end
 
+function player.gethealth()
+  return health - damage
+end
+
+function player.takedamage()
+  if not invincible then
+    damage = damage + 1
+    invincible = 3
+  end
+end
+
+function player.isdead()
+  return damage >= health
+end
+
 function player.update ()
+  if invincible then
+    invincible = invincible - FRAME
+    if invincible <= 0 then
+      invincible = nil
+    end
+  end
   local sum = vec2:new{}
   moving = false
   for key,dir in pairs(DIRS) do

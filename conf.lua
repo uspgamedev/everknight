a411bb9fb574d6843a03cc6942e2cd71e9c219e3
Kazuo256 <kazuo256@gmail.com>
2016-01-30 12:14:40 -0200

    FULSCREEEEEEEEEEEN

--- conf.lua
+++ conf.lua
@@ -4,5 +4,7 @@ function love.conf (t)
   t.window.width = 1024
   t.window.height = 768
   t.window.title = "GRIND ON MOTHAFUCKA"
+  t.window.fullscreen = true
+  t.window.fullscreentype = 'exclusive'
 end
 

ba10b010f4c641e703f534a52d64028af94ea7cc
Renan T. Carneiro <renanteruoc@gmail.com>
2016-01-30 12:25:19 -0200

    show player damage taken

--- Player.lua
+++ Player.lua
@@ -38,14 +38,29 @@ function Player:instance (obj)
   local atkdelay = 0
   local attacking = 0
 
+  function obj:load ()
+    -- print("load")
+    -- print (self.health)
+    self.health = 10
+    -- print (self.health)
+    self.damage = 0
+    self.displaylife = math.floor(player:gethealth() * blinglevel * 1.5)
+    -- behaviour
+  end
+
   function obj:takedamage (power, pos)
     -- print("yo")
     if not invincible then
+      local oldlife = math.floor(player:gethealth() * blinglevel * 1.5)--self.displaylife
       local shakebling = math.min(math.floor(blinglevel/10) + 1, #shakedur)
       print("ouch")
       -- print("")
       obj.damage = obj.damage + 1
       -- damage = damage + 1
+      self.displaylife = math.floor(player:gethealth() * blinglevel * 1.5)
+      lifediff = oldlife - self.displaylife
+      local posx, posy = self.getpos():unpack()
+      table.insert(displaynumbers,newnum(lifediff, {posx, posy - 1}))
       invincible = 1
       screenshake.intensity = blinglevel
       screenshake.duration = shakedur[shakebling]

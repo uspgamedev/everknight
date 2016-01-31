2332c5105393221fdfc98fc3a5bb6650b3f0dbc6
Kazuo256 <kazuo256@gmail.com>
2016-01-31 09:25:40 -0200

    Change game's name

--- conf.lua
+++ conf.lua
@@ -1,9 +1,9 @@
 
 function love.conf (t)
-  t.identity = 'ggj16'
+  t.identity = 'everknight'
   t.window.width = 1024
   t.window.height = 768
-  t.window.title = "GRIND ON MOTHAFUCKA"
+  t.window.title = "Everknight"
   t.window.fullscreen = true
   t.window.fullscreentype = 'exclusive'
 end

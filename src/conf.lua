70e1412749df244e47a60c3e3a00b87582e87cec
Kazuo256 <kazuo256@gmail.com>
2016-01-31 10:50:59 -0200

    Window icon

--- src/conf.lua
+++ src/conf.lua
@@ -1,9 +1,10 @@
 
 function love.conf (t)
   t.identity = 'everknight'
+  t.window.title = "Everknight"
+  t.window.icon = "assets/icon.png"
   t.window.width = 1024
   t.window.height = 768
-  t.window.title = "Everknight"
   t.window.fullscreen = true
   t.window.fullscreentype = 'exclusive'
 end
